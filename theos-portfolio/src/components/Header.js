b9216be0b334a329da77f05568b2cf073971f003
Theo Ward <theoward9@gmail.com>
2022-06-19 18:06:16 -0500

    added all pictures, name to header, centered projects

--- theos-portfolio/src/components/Header.js
+++ theos-portfolio/src/components/Header.js
@@ -1,35 +1,39 @@
 import React from "react";
 function Header({ currentPage, handlePageChange }) {
     return (
-        <ul className="nav nav-tabs" style={{backgroundColor: "#ECEDE9"}}>
-            <li className="nav-item">
-                <a
-                    href="#About"
-                    onClick={() => handlePageChange('About')}
-                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#Portfolio"
-                    onClick={() => handlePageChange('Portfolio')}
-                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                    Portfolio
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#Contact"
-                    onClick={() => handlePageChange('Contact')}
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </a>
-            </li>
-        </ul>
+        <div style={{backgroundColor: "#ECEDE9"}}>
+            <h2 className="p-1 mx-3 my-1" style={{backgroundColor: "#ECEDE9"}}>Theo Ward</h2>
+            <h6 className="p-1 mx-1 mb-1" style={{backgroundColor: "#ECEDE9"}}>Welcome to my portfolio!</h6>
+            <ul className="nav nav-tabs" style={{backgroundColor: "#ECEDE9"}}>
+                <li className="nav-item">
+                    <a
+                        href="#About"
+                        onClick={() => handlePageChange('About')}
+                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                    >
+                        About
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#Portfolio"
+                        onClick={() => handlePageChange('Portfolio')}
+                        className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Portfolio
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#Contact"
+                        onClick={() => handlePageChange('Contact')}
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 };
 
--- theos-portfolio/src/components/pages/Contact.js
+++ theos-portfolio/src/components/pages/Contact.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 
-function ContactForm (props) {
-    const [message, setMessage] = useState ('');
-    const [email, setEmail] = useState ('');
+function ContactForm(props) {
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [email, setEmail] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         props.onSubmit({
+            name: name,
             email: email,
             text: message,
         });
+        setName('');
         setEmail('');
         setMessage('');
+    };
+    const handleNameChange = (e) => {
+        setName(e.target.value);
     };
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -21,8 +27,18 @@ function ContactForm (props) {
     };
 
     return (
-        <div>
-             <form className="contact-form d-flex flex-column justify-content-end" onSubmit={handleSubmit}>
+        <div className='center'>
+            <h2 className='d-block text-center w-100 mt-2'>Please reach out!</h2>
+
+            <form className="contact-form d-flex flex-column mx-2" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    name="text"
+                    className="bucket-input w-75 mb-3 p-2 mt-3"
+                    onChange={handleNameChange}
+                ></input>
                 <input
                     type="text"
                     placeholder="Email"
@@ -31,14 +47,15 @@ function ContactForm (props) {
                     className="bucket-input w-75 mb-3 p-2 mt-3"
                     onChange={handleEmailChange}
                 ></input>
-                <input
+                <textarea
                     type="text"
                     placeholder="Message"
                     value={message}
                     name="text"
                     className="bucket-input w-75 mb-3 p-2"
                     onChange={handleMessageChange}
-                ></input>
+                    style={{height: 250}}
+                ></textarea>
                 <button className="contact-button w-25">Send</button>
             </form>
         </div>
