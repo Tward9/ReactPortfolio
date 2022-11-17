f5c1719811e70842c19a1afd8ced2773ad180779
Theo Ward <theoward9@gmail.com>
2022-11-17 17:04:52 -0600

    added html node and react to skills

--- theos-portfolio/src/components/pages/Resume.js
+++ theos-portfolio/src/components/pages/Resume.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import javascriptIcon from '../../skillsImages/JavaScript-logo.png'
+import javascriptIcon from '../../skillsImages/JavaScript-logo.png';
+import reactIcon from '../../skillsImages/react-logo.svg';
+import nodeIcon from '../../skillsImages/node-logo.png';
+import htmlIcon from '../../skillsImages/html-logo.png'
 export default function Resume() {
     return (
         <div className='d-flex mt-5 flex-wrap'>
-            <h2 className=' text-center w-100'>
+            <h2 className=' listCenter w-100'>
                 Skills Proficiencies!
             </h2>
-            <ul className='buttonCenter' style={{ listStyleType: 'none' }}>
-                <li>
+            <ul className='listCenter d-flex' style={{ listStyleType: 'none' }}>
+                <li className='px-2'>
                     <figure className='w-25'>
-                        <img className='figure-img roundImgSkill' id='javascriptIcon' src={javascriptIcon} alt='Javascript Icon, a J and S cutout of a yello block'/>
+                        <img className='figure-img roundImgSkill' id='javascriptIcon' src={javascriptIcon} alt='Javascript Icon, a J and S cutout of a yello block' />
                         <figcaption>
                             Javascript
+                        </figcaption>
+                    </figure>
+                </li>
+                <li className='px-2'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='reactIcon' src={reactIcon} alt='React Icon, a blue Fbolt' />
+                        <figcaption className='left'>
+                            React
                         </figcaption>
                     </figure>
                 </li>
@@ -48,7 +59,24 @@ export default function Resume() {
                     </ul>
                 </li> */}
             </ul>
-            
+            <ul className='listCenter d-flex' style={{ listStyleType: 'none' }}>
+                <li className='px-2'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='nodeIcon' src={nodeIcon} alt='Node Icon, a J and S inside a green hexagon' />
+                        <figcaption className='left'>
+                            Node
+                        </figcaption>
+                    </figure>
+                </li>
+                <li className='px-3'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='reactIcon' src={htmlIcon} alt='HTML Icon, a red shield with a 5 in the center' />
+                        <figcaption className='left'>
+                            HTML
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>
         </div>
     )
 }
