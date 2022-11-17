c4a284f1098e1f47b5b4f2e1797b028a462643fe
Theo Ward <theoward9@gmail.com>
2022-11-17 17:29:01 -0600

    added all skills need to work on alignment

--- theos-portfolio/src/components/pages/Resume.js
+++ theos-portfolio/src/components/pages/Resume.js
@@ -5,6 +5,10 @@ import nodeIcon from '../../skillsImages/node-logo.png';
 import htmlIcon from '../../skillsImages/html-logo.png';
 import graphqlIcon from '../../skillsImages/graphql-logo.png';
 import restIcon from '../../skillsImages/restapi-logo.jpeg';
+import cssIcon from '../../skillsImages/css-logo.png';
+import mongoDBIcon from '../../skillsImages/MongoDB-logo.png';
+import pythonIcon from '../../skillsImages/phyton-logo.jpeg';
+import sqlIcon from '../../skillsImages/sql-logo.png';
 export default function Resume() {
     return (
         <div className='d-flex mt-5 flex-wrap'>
@@ -36,38 +40,22 @@ export default function Resume() {
                         </figcaption>
                     </figure>
                 </li>
-                {/* <li className='skillsList text-center'>HTML</li>
-                <li className='skillsList'>
-                    Javascript
-                    <ul style={{ listStyleType: 'none' }}>
-                        <li>Node.JS</li>
-                        <li>Express.JS</li>
-                        <li>React.JS</li>
-                        <li>JSX</li>
-                    </ul>
+                <li className='px-2'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='reactIcon' src={sqlIcon} alt='sqL Icon, a blue cylinder' />
+                        <figcaption className=''>
+                            SQL
+                        </figcaption>
+                    </figure>
                 </li>
-                <li className='skillsList text-center'>MERN Stack</li>
-                <li className='skillsList text-center'>CSS</li>
-                <li className='skillsList'>
-                    mySQL
-                    <ul style={{ listStyleType: 'none' }}>
-                        <li>Sequelize</li>
-                    </ul>
+                <li className='px-2'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='reactIcon' src={mongoDBIcon} alt='MongoDB Icon, a leaf' />
+                        <figcaption className=''>
+                            MongoDB
+                        </figcaption>
+                    </figure>
                 </li>
-                <li className='skillsList'>
-                    NoSQL
-                    <ul style={{ listStyleType: 'none' }}>
-                        <li>MongoDB</li>
-                        <li>Mongoose</li>
-                    </ul>
-                </li>
-                <li className='skillsList'>
-                    API's
-                    <ul style={{ listStyleType: 'none' }}>
-                        <li>REST</li>
-                        <li>GraphQL</li>
-                    </ul>
-                </li> */}
             </ul>
             <ul className='listCenter d-flex' style={{ listStyleType: 'none' }}>
                 <li className='px-2'>
@@ -86,11 +74,27 @@ export default function Resume() {
                         </figcaption>
                     </figure>
                 </li>
+                <li className='px-3'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='reactIcon' src={cssIcon} alt='css Icon, a blue shield with a 3 in the center' />
+                        <figcaption className='left'>
+                            CSS
+                        </figcaption>
+                    </figure>
+                </li>
                 <li className='px-1'>
                     <figure className='w-25'>
                         <img className='figure-img roundImgSkill' id='reactIcon' src={restIcon} alt='REST API Icon, a a cloud with a gear in it' />
                         <figcaption className='left px-2'>
                             HTML
+                        </figcaption>
+                    </figure>
+                </li>
+                <li className='px-3'>
+                    <figure className='w-25'>
+                        <img className='figure-img roundImgSkill' id='reactIcon' src={pythonIcon} alt='Python Icon, a blue and yellow snake that form a plus' />
+                        <figcaption className='left'>
+                            Python
                         </figcaption>
                     </figure>
                 </li>
